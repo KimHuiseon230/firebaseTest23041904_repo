e673636913810948f5c6bcb4b1ae12ba306bedf1
Kim Huiseon <envy089@gmail.com>
2023-04-25 15:26:04 +0900

    [update]fourth commit

--- app/src/main/java/com/example/firebasetest23041904/PictureActivity.kt
+++ app/src/main/java/com/example/firebasetest23041904/PictureActivity.kt
@@ -3,18 +3,56 @@ package com.example.firebasetest23041904
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.firebasetest23041904.databinding.ActivityPictureBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 
 class PictureActivity : AppCompatActivity() {
     lateinit var binding: ActivityPictureBinding
+    lateinit var pictureAdapter: PictureAdapter
+    lateinit var  pictureDataList: MutableList<PictureData>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding =ActivityPictureBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        pictureDataList = mutableListOf()
+        pictureAdapter = PictureAdapter(this,pictureDataList)
+        binding.recyclerview.layoutManager =LinearLayoutManager(this)
+        binding.recyclerview.adapter= pictureAdapter
+
+        // 실시간 파이어베이스에서 pictureData 로딩하고, 스토리지에서 이미지 로딩
+        pictureDataLoading()
+        //사진첩 등록하기
         binding.pictureRegisterBtn.setOnClickListener {
             val intent = Intent(this@PictureActivity, PictureAddActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun pictureDataLoading() {
+        val pictureDAO = PictureDAO()
+        pictureDAO.pictureSelect()?.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                pictureDataList.clear()
+                for (data in snapshot.children) {
+                    val pictureData = data.getValue(PictureData::class.java)
+                    if (pictureData != null) {
+                        pictureDataList.add(pictureData)
+                    }
+                }
+                pictureAdapter.notifyDataSetChanged()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+             Toast.makeText(applicationContext,"데이터 로딩 실패",Toast.LENGTH_SHORT).show()
+                Log.e("PictureActivity","${error.message}")
+            }
+        })// 정보를 가져옴
     }
 }
--- app/src/main/java/com/example/firebasetest23041904/PictureAddActivity.kt
+++ app/src/main/java/com/example/firebasetest23041904/PictureAddActivity.kt
@@ -1,13 +1,12 @@
 package com.example.firebasetest23041904
-
 import android.app.Activity
 import android.content.Intent
 import android.net.Uri
-import android.os.Build.VERSION_CODES.P
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
-import android.provider.MediaStore.Audio.Media
+import android.util.Log
+import android.view.View
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import com.bumptech.glide.Glide
@@ -16,49 +15,69 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 class PictureAddActivity : AppCompatActivity() {
-    lateinit var binding: ActivityPictureAddBinding
-    var imgUri: Uri? = null
-
+    val binding by lazy { ActivityPictureAddBinding.inflate(layoutInflater) }
+    var imageUri: Uri? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityPictureAddBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        // 외부 저장소 접근 허용 여부 응답
-        val requestLauncher = registerForActivityResult(
-            ActivityResultContracts.StartActivityForResult()
-        ) {
-            if (it.resultCode == Activity.RESULT_OK) {
-                imgUri = it.data?.data
-                Glide.with(applicationContext).load(imgUri).into(binding.ivAddPicture)
-            }
-        }
 
+        // 외부저장소 접근 허용했는지 응답받음
+        val requestLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                if (it.resultCode == Activity.RESULT_OK) {
+                    imageUri = it.data?.data
+                    Glide.with(applicationContext).load(imageUri).into(binding.ivAddPicture)
+                }
+            }
         // 이벤트 접근
         binding.ivAddPicture.setOnClickListener {
             val intent = Intent(Intent.ACTION_PICK)
             intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*")
             requestLauncher.launch(intent)
         }
-
-        // fireStore 실시간 데이터 저장과 Storage 사진 저장
+        // FireStore에 실시간 데이터베이스 저장과 storage에 사진저장
         binding.btnPictureSave.setOnClickListener {
-            if (binding.ivAddPicture.drawable != null && binding.edtAddContent.text.isEmpty()) {
-                val pictureDAO = PictureDAO()
-                // 실시간 데이터에 있는 테이블에 Picture에 push 하여 키값을 등록되는 것을 가져옴
+            if (binding.ivAddPicture.drawable != null && binding.edtAddContent.text.isNotEmpty()) {
                 if (!MyApplication.checkAuth()) {
-                    Toast.makeText(applicationContext, "인증되지 않았습니다.", Toast.LENGTH_SHORT).show()
+                    // 실시간 데이터에 있는 테이블에 Picture에 push 하여 키값을 등록되는 것을 가져옴
+                    binding.progressBar.visibility = View.VISIBLE
+                    Toast.makeText(applicationContext, "인증이 안되었습니다.", Toast.LENGTH_SHORT).show()
                     val intent = Intent(applicationContext, MainActivity::class.java)
                     startActivity(intent)
                     finish()
                 }
+                val pictureDAO = PictureDAO()
+                // 실시간 테이블 picture에 push를 해서 key값을 등록하고 가져옴
                 val docID = pictureDAO.databaseReference?.push()?.key
                 val email = MyApplication.email
                 val content = binding.edtAddContent.text.toString().trim()
                 val date = SimpleDateFormat("yy-MM-dd HH.mm.ss").format(Date())
-                val pictureData = PictureData(docID,email,content,date)
-                // firebase storage 이미지 업로드 진행
-                pictureDAO.storage?.reference?.child("images/${docID}.png")
+                val pictureData = PictureData(docID, email, content, date)
+
+                // 실시간 DB Picture 테이블에 클래스 저장
+                pictureDAO.databaseReference?.child(docID!!)?.setValue(pictureData)?.addOnSuccessListener {
+                    Log.e("PictureAddActivity","pictureData를 picture 테이블에 입력성공")
+                    // 이미지를 스토리지에 저장
+                    // Firebase storage에 이미지 업로드 진행
+                    val pictureRef = pictureDAO.storage?.reference?.child("images/${docID}.png")
+                    pictureRef?.putFile(imageUri!!)?.addOnSuccessListener {
+                        binding.progressBar.visibility = View.INVISIBLE
+                        Toast.makeText(applicationContext, "이미지 업로드 성공", Toast.LENGTH_SHORT).show()
+                        Log.e("PictureAddActivity","이미지 업로드 성공")
+                        finish()
+                    }?.addOnFailureListener {
+                        binding.progressBar.visibility = View.INVISIBLE
+                        Toast.makeText(applicationContext, "이미지 업로드 실패", Toast.LENGTH_SHORT).show()
+                        Log.e("PictureAddActivity","이미지 업로드 실패")
+                    }
+                    // 이미지를 스토리지 저장 끝
+                }?.addOnFailureListener {
+                    Log.e("PictureAddActivity","pictureData를 picture 테이블에 입력실패")
+                }
+            } else {
+                binding.progressBar.visibility = View.INVISIBLE
+                Toast.makeText(applicationContext, "사진과 내용을 입력해주세요.", Toast.LENGTH_SHORT).show()
             }
-        } //end of btnPictureSave
+        }//end of btnPictureSave
     }
 }
